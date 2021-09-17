6032f206c82f35dcd34007aaa7b35acc24ae3f8d
pranavkp1661 <pranavp7645357@gmail.com>
2021-09-17 22:34:46 +0530

    window created

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+
+if __name__ == "__main__":
+    window = Tk()
+    # set the background colour of GUI window
+    window.configure(bg="light green")
+
+    # set the title of GUI window
+    window.title("Simple Calculator")
+
+    # set the configuration of GUI window
+    window.geometry("500x350")
+
+    window.mainloop()
