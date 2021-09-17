296f58bea1d87e29e8fdf8c05c9a104403e7992e
pranavkp1661 <pranavp7645357@gmail.com>
2021-09-17 23:15:30 +0530

    buttons added and result finished

--- main.py
+++ main.py
@@ -38,16 +38,84 @@ def clear():
 if __name__ == "__main__":
     window = Tk()
 
-    window.configure(bg="light green")
+    window.configure(bg="#E4E7C6")
 
     window.title("Simple Calculator")
 
-    window.geometry("500x350")
+    window.geometry("270x150")
 
     equation = StringVar()
 
     expression_field = Entry(window, textvariable=equation)
 
-    expression_field.grid(columnspan=4, ipadx=70)
+    expression_field.grid(columnspan=10, ipadx=100)
+
+    button1 = Button(window, text=' 1 ', fg='white', bg='#262725',
+                     command=lambda: press(1), height=1, width=7)
+    button1.grid(row=2, column=0)
+
+    button2 = Button(window, text=' 2 ', fg='white', bg='#262725',
+                     command=lambda: press(2), height=1, width=7)
+    button2.grid(row=2, column=1)
+
+    button3 = Button(window, text=' 3 ', fg='white', bg='#262725',
+                     command=lambda: press(3), height=1, width=7)
+    button3.grid(row=2, column=2)
+
+    button4 = Button(window, text=' 4 ', fg='white', bg='#262725',
+                     command=lambda: press(4), height=1, width=7)
+    button4.grid(row=3, column=0)
+
+    button5 = Button(window, text=' 5 ', fg='white', bg='#262725',
+                     command=lambda: press(5), height=1, width=7)
+    button5.grid(row=3, column=1)
+
+    button6 = Button(window, text=' 6 ', fg='white', bg='#262725',
+                     command=lambda: press(6), height=1, width=7)
+    button6.grid(row=3, column=2)
+
+    button7 = Button(window, text=' 7 ', fg='white', bg='#262725',
+                     command=lambda: press(7), height=1, width=7)
+    button7.grid(row=4, column=0)
+
+    button8 = Button(window, text=' 8 ', fg='white', bg='#262725',
+                     command=lambda: press(8), height=1, width=7)
+    button8.grid(row=4, column=1)
+
+    button9 = Button(window, text=' 9 ', fg='white', bg='#262725',
+                     command=lambda: press(9), height=1, width=7)
+    button9.grid(row=4, column=2)
+
+    button0 = Button(window, text=' 0 ', fg='white', bg='#262725',
+                     command=lambda: press(0), height=1, width=7)
+    button0.grid(row=5, column=0)
+
+    plus = Button(window, text=' + ', fg='black', bg='red',
+                  command=lambda: press("+"), height=1, width=7)
+    plus.grid(row=2, column=3)
+
+    minus = Button(window, text=' - ', fg='black', bg='red',
+                   command=lambda: press("-"), height=1, width=7)
+    minus.grid(row=3, column=3)
+
+    multiply = Button(window, text=' * ', fg='black', bg='red',
+                      command=lambda: press("*"), height=1, width=7)
+    multiply.grid(row=4, column=3)
+
+    divide = Button(window, text=' / ', fg='black', bg='red',
+                    command=lambda: press("/"), height=1, width=7)
+    divide.grid(row=5, column=3)
+
+    equal = Button(window, text=' = ', fg='black', bg='red',
+                   command=equalpress, height=1, width=7)
+    equal.grid(row=5, column=2)
+
+    clear = Button(window, text='Clear', fg='black', bg='red',
+                   command=clear, height=1, width=7)
+    clear.grid(row=5, column='1')
+
+    Decimal = Button(window, text='.', fg='white', bg='#262725',
+                     command=lambda: press('.'), height=1, width=7)
+    Decimal.grid(row=6, column=0)
 
     window.mainloop()
